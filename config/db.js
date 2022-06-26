e254370e0cf5949e083c6420bd33517f3fe2d569
dagutier-cntniall <dagutierrez@gmail.com>
2022-06-25 21:38:55 -0400

    Connection to Atlas, OK

--- config/db.js
+++ config/db.js
@@ -1,4 +1,5 @@
 module.exports =
 {
-    "URI": "mongodb://localhost/portfolio"
+    //"URI": "mongodb://localhost/portfolio"
+    "URI": "mongodb+srv://dagutierrez:W3b4ppd3vm1dt3rm@clusterdg0.vzqepbx.mongodb.net/portfolio?retryWrites=true&w=majority"
 }
